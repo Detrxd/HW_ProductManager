97dbd3701c27b46b52fbda6dfe9349b868007453
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-07-01 22:53:43 +0300

    Get 100% on branches - method 'searchBy'

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
 
     ProductManager manager = new ProductManager();
+    ProductRepository repository = new ProductRepository();
 
     private Book firstBook = new Book(1, "Гарри Поттер и узник Азкабана", 500, "Джоан Роулинг");
     private Book secondBook = new Book(2, "Зеленая миля ", 600, "Стивен Кинг");
@@ -38,6 +40,29 @@ class ProductManagerTest {
         Product[]actual = manager.searchBy("Nokia.inc");
         Product[]expected = new Product[]{};
         assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReceiveNull() {
+        manager.add(firstBook);
+        manager.add(secondBook);
+        repository.save(firstBook);
+        repository.save(secondBook);
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy("Пословицы");
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReceiveChosenText() {
+        manager.add(secondBook);
+        repository.save(secondBook);
+        assertArrayEquals(new Product[]{secondBook}, manager.searchBy("Зеленая миля"));
+
+
+        manager.add(firstBook);
+        repository.save(firstBook);
+        assertArrayEquals(new Product[]{firstBook}, manager.searchBy( "Гарри Поттер и узник Азкабана"));
     }
 
  }
